3f6ef9f5a57f12877a50f8d578968ea31978bc18
William Pitcock <nenolod@dereferenced.org>
2012-11-28 23:15:20 +0000

    dns/views: add views for zone list and record editor

--- panel2/dns/views.py
+++ panel2/dns/views.py
@@ -5,8 +5,9 @@ Copyright (c) 2012, 2013  TortoiseLabs, LLC.
 All rights reserved.
 """
 
-from flask import render_template
+from flask import render_template, Markup, redirect, url_for
 from panel2.models import User, get_session_user
+from panel2.dns.models import Domain, Record
 from panel2.dns import dns
 
 @dns.route('/')
@@ -14,3 +15,18 @@ from panel2.dns import dns
 def list():
     user = get_session_user()
     return render_template('dns/zones.html', zones=user.domains)
+
+@dns.route('/zone/<zone>')
+def view_zone(zone):
+    domain = Domain.query.filter_by(id=zone).first()
+    return render_template('dns/view-zone.html', zone=domain)
+
+@dns.route('/zone/<zone>/record/<record>', methods=['GET', 'POST'])
+def edit_record(zone, record):
+    record_obj = Record.query.filter_by(domain_id=zone, id=record).first()
+    if request.method == 'POST':
+        record_obj.set_name(request.form['name'])
+        record_obj.set_content(request.form['content'])
+        return redirect(url_for('.view_zone', zone=request_obj.id))
+
+    return render_template('dns/edit-record.html', zone=record_obj.domain, record=record_obj)
