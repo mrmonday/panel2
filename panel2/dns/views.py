aacf3f8bacfcfcc3e2ab819e81f6234b4422f5cd
William Pitcock <nenolod@dereferenced.org>
2012-11-28 21:01:41 +0000

    Add some base scaffolding for the DNS list view.

--- panel2/dns/views.py
+++ panel2/dns/views.py
@@ -5,3 +5,12 @@ Copyright (c) 2012, 2013  TortoiseLabs, LLC.
 All rights reserved.
 """
 
+from flask import render_template
+from panel2.models import User, get_session_user
+from panel2.dns import dns
+
+@dns.route('/')
+@dns.route('/zones')
+def list():
+    user = get_session_user()
+    return render_template('dns/zones.html', zones=user.domains)
