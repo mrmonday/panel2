a5a7d18fc59554534fbd04fa67e528456af0c945
William Pitcock <nenolod@dereferenced.org>
2012-11-29 21:49:27 +0000

    Begin support module.

---
+++ panel2/support/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2012, 2013  TortoiseLabs, LLC.
+
+All rights reserved.
+"""
+
+from flask import Blueprint
+
+support = Blueprint('support', __name__, template_folder='templates')
+
