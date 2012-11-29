eda3a832a7263d9cc5a74bd8843d4ae3c108d8ce
William Pitcock <nenolod@dereferenced.org>
2012-11-29 01:05:13 +0000

    DNS cleanups.

--- panel2/dns/views.py
+++ panel2/dns/views.py
@@ -17,18 +17,18 @@ def list():
     user = get_session_user()
     return render_template('dns/zones.html', zones=user.domains)
 
-@dns.route('/zone/<zone>')
-def view_zone(zone):
-    domain = Domain.query.filter_by(id=zone).first()
+@dns.route('/zone/<zone_id>')
+def view_domain(zone_id):
+    domain = Domain.query.filter_by(id=zone_id).first()
     return render_template('dns/view-zone.html', zone=domain)
 
-@dns.route('/zone/<zone>/record/<record>', methods=['GET', 'POST'])
-def edit_record(zone, record):
-    record_obj = Record.query.filter_by(domain_id=zone, id=record).first()
+@dns.route('/zone/<zone_id>/record/<record_id>', methods=['GET', 'POST'])
+def edit_record(zone_id, record_id):
+    record_obj = Record.query.filter_by(domain_id=zone_id, id=record_id).first()
     if request.method == 'POST':
         record_obj.set_name(request.form['subdomain'] + '.' + record_obj.domain.name)
         record_obj.set_content(request.form['content'])
-        return redirect(url_for('.view_zone', zone=request_obj.id))
+        return redirect(url_for('.view_domain', zone_id=request_obj.id))
 
     return render_template('dns/edit-record.html', zone=record_obj.domain, record=record_obj)
 
@@ -37,7 +37,7 @@ def new_domain():
     if request.method == 'POST':
         user = get_session_user()
         domain = Domain(user, request.form['domain_name'])
-        return redirect(url_for('.view_zone', zone=domain.id))
+        return redirect(url_for('.view_domain', zone_id=domain.id))
 
     return render_template('dns/new-domain.html')
 
@@ -48,7 +48,7 @@ def new_record(zone_id):
         domain.add_record(request.form['subdomain'] + '.' + domain.name,
                           request.form['content'], request.form['type'],
                           request.form['prio'], request.form['ttl'])
-        return redirect(url_for('.view_zone', zone=domain.id))
+        return redirect(url_for('.view_domain', zone_id=domain.id))
 
     return render_template('dns/new-record.html', zone=domain)
 
@@ -57,4 +57,21 @@ def delete_record(zone_id, record_id):
     Record.query.filter_by(domain_id=zone_id, id=record_id).delete()
     db.session.commit()
 
-    return redirect(url_for('.view_zone', zone=zone_id))
+    return redirect(url_for('.view_domain', zone_id=zone_id))
+
+@dns.route('/zone/<zone_id>/delete')
+def delete_domain(zone_id):
+    domain = Domain.query.filter_by(id=zone_id).first()
+
+    # Surprise, surprise!  The SQLAlchemy documentation lies.
+    # Remove all dependent records in a separate transaction before
+    # removing the parent.  This way we can avoid MySQL derping.
+    for record in domain.records:
+        db.session.delete(record)
+
+    db.session.commit()
+
+    db.session.delete(domain)
+    db.session.commit()
+
+    return redirect(url_for('.list'))
