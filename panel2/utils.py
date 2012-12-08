49d5311fbbbade7ec65554662c2965179348e3c6
William Pitcock <nenolod@dereferenced.org>
2012-12-08 06:20:36 +0000

    panel2/utils: add simple e-mail sending API (closes #13)

--- panel2/utils.py
+++ panel2/utils.py
@@ -5,7 +5,7 @@ Copyright (c) 2012, 2013  TortoiseLabs, LLC.
 All rights reserved.
 """
 
-from panel2 import db
+from panel2 import db, mail, app
 
 class CommitableMixIn(object):
     def commit(self):
@@ -20,3 +20,9 @@ def is_email_valid(email):
     if '@' not in s:
         return False
     return True
+
+def send_simple_email_list(recipients, subject, message):
+    mail.send_message(subject=subject, body=message, recipients=recipients, sender=app.config['NOREPLY_MAIL'])
+
+def send_simple_email(recipient, subject, message):
+    send_simple_email_list([recipient], subject, message)
