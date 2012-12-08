97d91a4e40305511c837638ef67dee692d1266c9
William Pitcock <nenolod@dereferenced.org>
2012-12-08 08:04:11 +0000

    panel2/auth_hooks: fix API violations

--- panel2/auth_hooks.py
+++ panel2/auth_hooks.py
@@ -11,11 +11,11 @@ from flask import request
 
 @login_signal.connect_via(app)
 def send_login_notice(*args, **kwargs):
-    user = kwargs.pop('user', none)
-    user.send_email('Authentication successful for %s from %s' % (user.name, request.environ['REMOTE_ADDR']), 'email/auth-message.txt')
+    user = kwargs.pop('user', None)
+    user.send_email('Authentication successful for %s from %s' % (user.username, request.environ['REMOTE_ADDR']), 'email/auth-message.txt')
 
 @authfail_signal.connect_via(app)
 def send_authfail_notice(*args, **kwargs):
-    user = kwargs.pop('user', none)
-    user.send_email('Authentication failed for %s from %s' % (user.name, request.environ['REMOTE_ADDR']), 'email/authfail-message.txt')
+    user = kwargs.pop('user', None)
+    user.send_email('Authentication failed for %s from %s' % (user.username, request.environ['REMOTE_ADDR']), 'email/authfail-message.txt')
 
