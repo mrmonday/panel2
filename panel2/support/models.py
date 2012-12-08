b0f6b44e9ae331bb7703f6e71de579b69e132b7b
William Pitcock <nenolod@dereferenced.org>
2012-12-08 08:09:37 +0000

    panel2/support/models: add signals for ticket create and reply

--- panel2/support/models.py
+++ panel2/support/models.py
@@ -8,6 +8,10 @@ All rights reserved.
 from panel2 import app, db
 
 import time
+import blinker
+
+ticket_create_signal = blinker.Signal('A signal which is fired when a ticket is created')
+ticket_reply_signal = blinker.Signal('A signal which is fired when a reply is added')
 
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,13 +42,15 @@ class Ticket(db.Model):
         db.session.add(self)
         db.session.commit()
 
-        self.add_reply(self.user, message)
+        self.add_reply(self.user, message, False)
+
+        ticket_create_signal.send(app, ticket=self)
 
     def __repr__(self):
         return "<Ticket: %d - '%s'>" % (self.id, self.subject)
 
-    def add_reply(self, from_user, message):
-        return Reply(self, from_user, message)
+    def add_reply(self, from_user, message, fire_reply_signal=True):
+        return Reply(self, from_user, message, fire_reply_signal)
 
     def close(self):
         self.closed_at = time.time()
@@ -64,7 +70,7 @@ class Reply(db.Model):
     ticket = db.relationship('Ticket', backref='replies')
     from_user = db.relationship('User')
 
-    def __init__(self, ticket, from_user, message):
+    def __init__(self, ticket, from_user, message, fire_reply_signal=True):
         self.ticket = ticket
         self.ticket_id = ticket.id
 
@@ -77,6 +83,9 @@ class Reply(db.Model):
 
         db.session.add(self)
         db.session.commit()
+
+        if fire_reply_signal is True:
+            ticket_reply_signal.send(app, ticket=self.ticket, reply=self)
 
     def __repr__(self):
         return "<Reply for Ticket %d, '%s...'>" % (self.ticket.id, self.message[0:50])
