97e63f2f2ad301ae58dd47fa9b93237c40719142
William Pitcock <nenolod@dereferenced.org>
2012-12-10 13:14:04 +0000

    panel2/service: add IPAddress, IPAddressRef, and Service.attach_ip().

--- panel2/service.py
+++ panel2/service.py
@@ -41,3 +41,73 @@ class Service(db.Model):
     def invoice(self, invoice):
         pass
 
+    def attach_ip(self, ip):
+        return IPAddressRef(ip, self.user, self)
+
+class IPAddress(db.Model):
+    __tablename__ = 'ips'
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(255))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref='ips')
+
+    service_id = db.Column(db.Integer, db.ForeignKey('services.id'))
+    service = db.relationship('Service', backref='services')
+
+    def __init__(self, ip, user=None, service=None):
+        self.ip = ip
+
+        if user is not None:
+            self.update_user(user)
+
+        if service is not None:
+            self.update_service(service)
+
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        base = "<IP: %s" % self.ip
+
+        if self.service is not None:
+            base = base + " [%s]" % repr(self.service)
+
+        if self.user is not None:
+            base = base + " {%s}" % repr(self.user)
+
+        base = base + ">"
+        return base
+
+    def update_service(self, service):
+        if self.service == service:
+            return
+
+        self.service_id = service.id
+        self.service = service
+
+        db.session.add(self)
+        db.session.commit()
+
+    def update_user(self, user):
+        if self.user == user:
+            return
+
+        self.user_id = user.id
+        self.user = user
+
+        db.session.add(self)
+        db.session.commit()
+
+def IPAddressRef(ip, user=None, service=None):
+    '''A wrapper around the IPAddress constructor which handles lookup as well as
+       creation of IP address records.'''
+    ip_obj = IPAddress.query.filter_by(ip=ip).first()
+    if ip_obj is not None:
+        if user is not None:
+            ip_obj.update_user(user)
+        if service is not None:
+            ip_obj.update_service(service)
+        return ip_obj
+
+    return IPAddress(ip, user, service)
