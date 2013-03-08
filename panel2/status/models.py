2c44983f0587f2e2314156b10c731b136586c374
William Pitcock <nenolod@dereferenced.org>
2013-03-08 16:51:52 +0000

    status: api-enable

--- panel2/status/models.py
+++ panel2/status/models.py
@@ -67,6 +67,11 @@ class Incident(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def _serialize(self):
+        return dict(incident=self.id, subject=self.subject, user=self.user.username, priority=self.priority,
+                    department=self.department, is_open=self.is_open, opened_at=self.opened_at,
+                    closed_at=self.closed_at, replies=[reply._serialize() for reply in self.replies])
+
 class IncidentReply(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     incident_id = db.Column(db.Integer, db.ForeignKey('incident.id'))
@@ -97,3 +102,7 @@ class IncidentReply(db.Model):
 
     def __repr__(self):
         return "<IncidentReply for Incident %d, '%s...'>" % (self.ticket.id, self.message[0:50])
+
+    def _serialize(self):
+        return dict(incident=self.incident_id, user=self.from_user.username, message=self.message,
+                    replied_at=self.replied_at)
