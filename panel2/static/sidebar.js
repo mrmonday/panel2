97cd51b5f711b33953398462adc94017c44c8ea6
William Pitcock <nenolod@dereferenced.org>
2013-03-18 08:38:11 +0000

    Add missing template files

---
+++ panel2/static/sidebar.js
@@ -0,0 +1,13 @@
+// Sticky sidebar w/jQuery
+$(document).ready(function() {
+	sidebarwidth = $(".row-fluid .span2").css('width');
+	bodypaddingtop = $("header").css('height');
+	sidebarheight = $("body").css('height');
+	$('#sidebar').css('width', sidebarwidth);
+	$('#sidebar').css('height', sidebarheight);
+	contentmargin = parseInt(sidebarwidth);
+	$('#container-base').css('paddingTop', bodypaddingtop);
+	$('.contentfix').css('marginLeft', contentmargin + 20);
+	$('.contentfix').css('marginRight', 0);
+});
+
