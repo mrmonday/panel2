63bc240485b7b9a522f9ff0ae1952823db3d58f6
William Pitcock <nenolod@dereferenced.org>
2013-04-11 12:42:35 +0000

    Revert "vps: if you already have an account please sign in"
    This reverts commit ec1db202284ecd838b98f9608a545bc90cc64fd3.

--- start-uwsgi.sh
+++ start-uwsgi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "HOME is ${HOME}"
-(sleep 2; chmod 777 ${HOME}/panel2-edge-uwsgi.sock) &
-/usr/bin/env uwsgi --workers 4 --plugin-dir /usr/lib/uwsgi --plugin python -s ${HOME}/panel2-edge-uwsgi.sock -w panel2:app
+(sleep 2; chmod 777 ${HOME}/panel2-uwsgi.sock) &
+/usr/bin/env uwsgi --workers 4 --plugin-dir /usr/lib/uwsgi --plugin python -s ${HOME}/panel2-uwsgi.sock -w panel2:app
