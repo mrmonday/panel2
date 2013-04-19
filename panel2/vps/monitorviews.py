70d6ecd8683fc654593e302afb026edf915b0369
William Pitcock <nenolod@dereferenced.org>
2013-04-19 04:05:27 +0000

    vps: initial work on monitoring frontend

--- panel2/vps/monitorviews.py
+++ panel2/vps/monitorviews.py
@@ -16,10 +16,31 @@ from the use of this software.
 import rrdtool, os, time, subprocess, socket
 from flask import render_template
 from panel2 import app, db, cron
+from panel2.vps import vps
 from panel2.vps.models import Node, XenVPS
 from panel2.cron import MONITORING
-from panel2.utils import send_simple_email
+from panel2.utils import send_simple_email, render_template_or_json
 from ediarpc.rpc_client import ServerProxy
 from panel2.vps.monitorengine import *
+from panel2.user import login_required, admin_required, get_session_user, User, Session
+from panel2.vps.views import can_access_vps
 
+@vps.route('/<vps>/monitors')
+@login_required
+def monitor_list(vps):
+    vps = XenVPS.query.filter_by(id=vps).first_or_404()
+    if can_access_vps(vps) is False:
+        abort(403)
+    return render_template_or_json('vps/monitoring-base.html', service=vps)
+
+@vps.route('/<vps>/monitor/<monitor>')
+@login_required
+def monitor_rules(vps, monitor):
+    vps = XenVPS.query.filter_by(id=vps).first_or_404()
+    if can_access_vps(vps) is False:
+        abort(403)
+    monitor = MonitorProbe.query.filter_by(id=monitor).first_or_404()
+    if monitor not in vps.probes:
+        abort(403)
+    return render_template_or_json('vps/monitoring-details.html', service=vps, monitor=monitor)
 
