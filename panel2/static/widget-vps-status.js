78e42d158e20d39a400e6559f8ea30110464b138
William Pitcock <nenolod@dereferenced.org>
2013-06-25 21:39:18 +0000

    status widget: move data to inside closure for reentrancy

--- panel2/static/widget-vps-status.js
+++ panel2/static/widget-vps-status.js
@@ -1,28 +1,26 @@
-var icon_checking = $('<i class="icon-spinner icon-spin"></i>');
-var icon_running  = $('<i class="icon-ok-sign text-success"></i>');
-var icon_stopped  = $('<i class="icon-remove-sign text-error"></i>');
-
-function status_fetch(url, target) {
-	function __data_received(data) {
-		if (data.running)
-			target.html(icon_running);
-		else
-			target.html(icon_stopped);
-	}
-
-	$.ajax({
-		url: url,
-		type: 'GET',
-		dataType: 'json',
-		success: __data_received,
-	});
-}
-
 function status_setup(url, target) {
-	target.html(icon_checking);
+	var icon_checking = $('<i class="icon-spinner icon-spin"></i>');
+	var icon_running  = $('<i class="icon-ok-sign text-success"></i>');
+	var icon_stopped  = $('<i class="icon-remove-sign text-error"></i>');
+
+	var tg = target;
+
+	tg.html(icon_checking);
 
 	function __fetch() {
-		status_fetch(url, target);
+		function __data_received(data) {
+			if (data.running)
+				tg.html(icon_running);
+			else
+				tg.html(icon_stopped);
+		}
+
+		$.ajax({
+			url: url,
+			type: 'GET',
+			dataType: 'json',
+			success: __data_received,
+		});
 	}
 
 	__fetch();
