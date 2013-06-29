8c93685cab339e3713fd1ddffec5fa6cd9600627
William Pitcock <nenolod@dereferenced.org>
2013-06-29 08:46:44 +0000

    ui: replace jquery.eqheight with more efficient homemade solution

--- panel2/static/sidebar.js
+++ panel2/static/sidebar.js
@@ -1,5 +1,38 @@
+function equalize(p) {
+	function equalize_worker() {
+		var maxheight = 0;
+
+		/* find maximum height */
+		p.children().each(function() {
+			var height = $(this).height("auto").height();
+			if (height > maxheight)
+				maxheight = height;
+		});
+
+		/* apply maximum height */
+		p.children().each(function() {
+			$(this).height(maxheight);
+		});
+	}
+
+	equalize_worker();
+}
+
 $(document).ready(function() {
-	$(".row-equalize").eqHeight(".span1, .span2, .span3, .span4, .span5, .span6, .span7, .span8, .span9, .span10, .span11, .span12");
+	$(".row-equalize").each(function() {
+		var maxheight = 0;
+		var recurse = false;
+
+		equalize($(this));
+		$(this).bind("DOMSubtreeModified", function() {
+			if (recurse)
+				return;
+
+			recurse = true;
+			equalize($(this));
+			recurse = false;
+		});
+	});
 
 	$('.launchconsole').click(function() {
 		window.open($(this).attr('href'),'', 'scrollbars=no,location=no,status=no,toolbar=no,menubar=no,width=640,height=460');
