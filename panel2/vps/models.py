10bc952d40ce92fdd2581a5964489e9322d3e897
William Pitcock <nenolod@dereferenced.org>
2013-08-06 09:44:16 +0000

    vps: make it possible to hide regions

--- panel2/vps/models.py
+++ panel2/vps/models.py
@@ -111,6 +111,7 @@ class KernelProfileArgument(db.Model):
 class Region(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    hidden = db.Column(db.Boolean, default=False)
 
     def __init__(self, name):
         self.name = name
