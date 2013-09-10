27e0293f49673f675a763a0887faed05d9c83c7b
William Pitcock <nenolod@dereferenced.org>
2013-09-10 12:43:13 +0000

    start-uwsgi: run uwsgi in master mode

--- start-uwsgi.sh
+++ start-uwsgi.sh
@@ -2,4 +2,4 @@
 
 echo "HOME is ${HOME}"
 (sleep 2; chmod 777 ${HOME}/panel2-uwsgi.sock) &
-/usr/bin/env uwsgi --workers 8 --plugin-dir /usr/lib/uwsgi --plugin python -s ${HOME}/panel2-uwsgi.sock -w panel2:app
+/usr/bin/env uwsgi --master --workers 8 --plugin-dir /usr/lib/uwsgi --plugin python -s ${HOME}/panel2-uwsgi.sock -w panel2:app
