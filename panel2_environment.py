9d91df86c62330d258125107e895238f470269a8
William Pitcock <nenolod@dereferenced.org>
2013-12-01 17:20:04 +0000

    environment: simplify further

--- panel2_environment.py
+++ panel2_environment.py
@@ -28,20 +28,14 @@ from panel2.profile import profile
 from panel2.mod_branding_tortoiselabs import branding
 
 # register the branding module as a blueprint
-application.register_blueprint(branding, subdomain='manage', url_prefix='/branding')
+application.register_blueprint(branding, url_prefix='/branding')
+application.register_blueprint(dns, url_prefix='/dns')
+application.register_blueprint(support, url_prefix='/support')
+application.register_blueprint(vps, url_prefix='/vps')
+application.register_blueprint(invoice, url_prefix='/invoice')
 
-application.add_url_rule('/static/<path:filename>',
-			endpoint='static',
-			subdomain='manage',
-			view_func=application.send_static_file)
-
-application.register_blueprint(dns, subdomain='manage', url_prefix='/dns')
-application.register_blueprint(support, subdomain='manage', url_prefix='/support')
-application.register_blueprint(vps, subdomain='manage', url_prefix='/vps')
-application.register_blueprint(invoice, subdomain='manage', url_prefix='/invoice')
-
-application.register_blueprint(status, subdomain='manage', url_prefix='/status')
-application.register_blueprint(profile, subdomain='manage', url_prefix='/account')
+application.register_blueprint(status, url_prefix='/status')
+application.register_blueprint(profile, url_prefix='/account')
 
 nav.register('DNS', 'icon-globe', 'dns.list')
 nav.register('vServers', 'icon-cloud', 'vps.list')
