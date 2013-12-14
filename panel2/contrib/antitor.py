3f4f69ec897412cff573f29014fcd7679d5c5495
William Pitcock <nenolod@dereferenced.org>
2013-12-14 21:09:41 +0000

    antitor: improve suspension logic

--- panel2/contrib/antitor.py
+++ panel2/contrib/antitor.py
@@ -9,12 +9,17 @@ def antitor_suspend():
     data = requests.get('http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv')
     ips = data.text.split('\n')
 
+    print 'suspending tor nodes'
+
     ctx = app.test_request_context()
     ctx.push()
 
     for ip in ips:
         ipa = IPAddress.query.filter_by(ip=ip).first()
-        if not ipa or not ipa.service.is_entitled or ipa.service.tor_whitelisted:
+        if not ipa or ipa.service.tor_whitelisted:
+            continue
+        if not ipa.service.is_entitled:
+            ipa.service.destroy()
             continue
         ipa.service.suspend(disable_renew=True)
 
